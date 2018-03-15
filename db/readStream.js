e802ffbe5bd2d85d336a25410c5dc97dbf785b40
Becca Stanger <beccastanger@gmail.com>
2018-03-14 22:44:54 -0700

    Add read stream and pipe processing

---
+++ db/readStream.js
@@ -0,0 +1,22 @@
+const Readable = require('stream').Readable;
+const util = require('util');
+const FakerModel = require('./models/faker-restaurant-model.js');
+
+var ReadStream = function(dbSize) {
+    Readable.call(this, { objectMode: true });
+    this.data;
+    this.id = 0;
+    this.dbSize = dbSize;
+};
+
+util.inherits(ReadStream, Readable);
+
+ReadStream.prototype._read = function() {
+    if (this.id === this.dbSize) {
+        return this.push(null);
+    }
+    var data = new FakerModel(this.id++);
+    this.push(JSON.stringify(data));
+};
+
+module.exports = ReadStream;
--- db/seedJSON.js
+++ db/seedJSON.js
@@ -1,15 +1,10 @@
 const fs = require('fs');
 const faker = require('faker');
 const bluebird = require('bluebird');
-const fakerModel = require('./models/faker-restaurant-model.js');
-
-const Readable = require('stream').Readable;
-const util = require('util');
+const ReadStream = require('./readStream.js');
 
 let fullUrls = [];
-let id = 1;
-// const dbSize = 10000000;
-const dbSize = 80;
+const dbSize = 100000;
 
 function initialize() {
   faker.seed(123);
@@ -24,40 +19,10 @@ function initialize() {
 }
 
 function createAllRestaurants(){
-  var ReadStream = function() {
-      Readable.call(this, {
-          objectMode: true
-      });
-      this.data;
-      this.id = 0;
-  };
-  util.inherits(ReadStream, Readable);
-  ReadStream.prototype._read = function() {
-      if (this.id === 80) {
-          return this.push(null);
-      }
-      var data = new fakerModel(this.id);
-      console.log('read             : ' + JSON.stringify(data));
-      this.push(JSON.stringify(data));
-  };
-  var rs = new ReadStream();
+  var rs = new ReadStream(dbSize);
   const file = fs.createWriteStream('./data/output.json');
   rs.pipe(file);
-  // for(id; id <= dbSize; id++){
-  //   console.log(new fakerModel(id));
-  //
-  //
-  //   // readableSrc.pipe(writableDest)
-  //   // fs.createReadStream(file)
-  //     // .pipe(fs.createWriteStream('./data/output.json'))
-  //     // .on('finish', function(){ console.log('Done'); });
-  // }
 }
-
-// var data = require('./test_data.json'),
-// var Readable = require('stream').Readable;
-// var util = require('util')
-
 
 function createUrlArray() {
   return new Promise(function(resolve, reject){

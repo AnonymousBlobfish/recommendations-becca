dc13a19a990b4542422f205c88796c60d3a2e608
Becca Stanger <beccastanger@gmail.com>
2018-03-19 16:49:41 -0700

    Attempt to fix MySQL server changes necessary

--- db/models/mysql.js
+++ db/models/mysql.js
@@ -2,7 +2,7 @@ const db = require('../sqlConnection.js');
 const con = db.connection();
 
 const restaurants = {
-  findOne: function(restaurant_id){
+  findOneBasic: function(restaurant_id){
     return new Promise(function(resolve, reject){
       con.query(`SELECT restaurant_id, name FROM restaurants WHERE restaurant_id = ${restaurant_id};`, function(err, results) {
         if (err) {
@@ -10,6 +10,38 @@ const restaurants = {
           reject(err);
         } else {
           resolve(results);
+        }
+      });
+    });
+  },
+
+  findOneExtendedWithPhotos: function(restaurant_id){
+    return new Promise(function(resolve, reject){
+      con.query(`SELECT * FROM restaurants WHERE restaurant_id = ${restaurant_id};`, function(err, result) {
+        if (err) {
+          console.log(err); // Should send back 500
+          reject(err);
+        } else {
+          console.log('result of search for nearby restaurant info is ', result);
+          // let nearbyRestIdresult[0].restaurant_id;
+          console.log('restaurant Id is ', result[0].restaurant_id);
+          photos.find(result[0].restaurant_id)
+            .then(photoUrls => {
+              let photoArr = [];
+              for (var i = 0; i < photoUrls.length; i++){
+                photoArr.push(photoUrls[i].photo_url);
+              }
+              result.photos = photoArr;
+              resolve(result);
+              // if(i === (nearbyRests.length - 1)){
+              //   console.log('resolved');
+              //   resolve(nearbyRests);
+              // }
+              // console.log('nearbyRest length is... ', nearbyRests.length);
+            }).catch(err => {
+              reject(err);
+            })
+          // resolve(results);
         }
       });
     });
@@ -27,9 +59,38 @@ const restaurants = {
       // restIdsString += ')';
       // console.log('restIdsString is ', restIdsString);
       console.log(restaurantArr[0].nearby_id);
-      restaurants.findOne(restaurantArr[0].nearby_id)
-        .then(result => {
-          console.log('result on finding first restaurant is ', result);
+
+      var nearbyResults = [];
+
+      restaurants.findOneExtendedWithPhotos(50)
+        .then(result1 => {
+          console.log('result after first call is ', result1);
+          nearbyResults.push(result1);
+          restaurants.findOneExtendedWithPhotos(40)
+            .then(result2 => {
+              nearbyResults.push(result2);
+            });
+        }).then(() => {
+          restaurants.findOneExtendedWithPhotos(30)
+            .then(result3 => {
+              nearbyResults.push(result3);
+            });
+        }).then(() => {
+          restaurants.findOneExtendedWithPhotos(20)
+            .then(result4 => {
+              nearbyResults.push(result4);
+            });
+        }).then(() => {
+          restaurants.findOneExtendedWithPhotos(10)
+            .then(result5 => {
+              nearbyResults.push(result5);
+            });
+        }).then(() => {
+          restaurants.findOneExtendedWithPhotos(60)
+            .then(result6 => {
+              nearbyResults.push(result6);
+              resolve(nearbyResults)
+            });
         }).catch(err => {
           console.log(err);
         })
@@ -94,7 +155,7 @@ const photos = {
   }
 }
 
-exports.findOneRestaurant = restaurants.findOne;
+exports.findOneRestaurant = restaurants.findOneBasic;
 exports.findManyRestaurants = restaurants.findManyRestaurants;
 exports.findNearbys = nearbys.find;
 exports.findPhotos = photos.find;
